b90c2bdeac712ace6eba87d8b74423e65c6bf990
Bastien Tran <bastien.tran@gmail.com>
2019-01-16 19:40:07 +0100

    got the green peps to disappear/reappear in the note

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,10 +1,32 @@
 //
-$(".mushroom").hide();
-$(".pep").hide();
-$(".green-pepper").hide();
-$(".btn-mushrooms").removeClass("active");
-$(".btn-green-peppers").removeClass("active");
-$(".btn-pepperonni").removeClass("active");
+// $(".mushroom").hide();
+// $(".pep").hide();
+// $(".green-pepper").hide();
+// $(".btn-mushrooms").removeClass("active");
+// $(".btn-green-peppers").removeClass("active");
+// $(".btn-pepperonni").removeClass("active");
+$(".btn-crust").removeClass("active");
+$(".btn-sauce").removeClass("active");
+
+if ($(".green-pepper").is(":hidden")) {
+  $(".btn-green-peppers").removeClass("active");
+} else {
+  $(".btn-green-peppers").addClass("active");
+}
+
+if ($(".mushroom").is(":hidden")) {
+  $(".btn-mushrooms").removeClass("active");
+} else {
+  $(".btn-mushrooms").addClass("active");
+}
+
+if ($(".pep").is(":hidden")) {
+  $(".btn-pepperonni").removeClass("active");
+} else {
+  $(".btn-pepperonni").addClass("active");
+}
+
+//$( "div:contains('John')" )
 
 // setting crust to crust only
 $(".crust").removeClass("crust-gluten-free");
@@ -12,8 +34,10 @@ $(".crust").removeClass("crust-gluten-free");
 $(".btn-crust").click(function() {
   if ($(".crust-gluten-free")[0]) {
     $(".crust").removeClass("crust-gluten-free");
+    $(".btn-crust").removeClass("active");
   } else {
     $(".crust").addClass("crust-gluten-free");
+    $(".btn-crust").addClass("active");
   }
 });
 
@@ -23,8 +47,10 @@ $(".sauce").removeClass("sauce-white");
 $(".btn-sauce").click(function() {
   if ($(".sauce-white")[0]) {
     $(".sauce").removeClass("sauce-white");
+    $(".btn-sauce").removeClass("active");
   } else {
     $(".sauce").addClass("sauce-white");
+    $(".btn-sauce").addClass("active");
   }
 });
 
@@ -32,8 +58,10 @@ $(".btn-green-peppers").click(function() {
   $(".green-pepper").toggle();
   if ($(".green-pepper").is(":hidden")) {
     $(".btn-green-peppers").removeClass("active");
+    $("li:contains('green peppers')").hide();
   } else {
     $(".btn-green-peppers").addClass("active");
+    $("li:contains('green peppers')").show();
   }
 });
 
@@ -47,8 +75,8 @@ $(".btn-mushrooms").click(function() {
 });
 
 $(".btn-pepperonni").click(function() {
-  $(".pepperonni").toggle();
-  if ($(".pepperonni").is(":hidden")) {
+  $(".pep").toggle();
+  if ($(".pep").is(":hidden")) {
     $(".btn-pepperonni").removeClass("active");
   } else {
     $(".btn-pepperonni").addClass("active");
